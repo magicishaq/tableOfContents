7d56074d3413a5d2744c0ca2a5fd9572bd3d8959
magicishaq <ishaqkhan1993@gmail.com>
2021-10-29 13:05:34 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import MainContent from './components/MainContent'
+import Layout from './components/Layout'
+import TableOfContents from './components/TableOfContents'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <MainContent />
+      <TableOfContents />
+    </Layout>
+    
   );
 }
 
---
+++ src/components/MainContent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const MainContent = () => {
+    return(
+        <article>
+            <div className="container">
+                <div className="item"></div>
+                <div className="item"></div>
+                <div className="item"></div>
+            </div>
+        </article>
+    )
+}
+
+export default MainContent; 
